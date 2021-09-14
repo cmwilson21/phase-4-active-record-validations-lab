ba00a4c531e1a995699d13d19d4cd647cba33e6f
Courtney Wilson <cmwilson132@gmail.com>
2021-09-13 21:35:52 -0500

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+  validate :clickbait?
+
+  BAIT = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if BAIT.none? {|b| b.match title }
+      errors.add(:title, "Must be more clickbaity")
+    end
+  end
+
+
 end
